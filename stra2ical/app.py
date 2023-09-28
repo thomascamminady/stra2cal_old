f7e6054a9466337e6abe6e34eaf23c1906f896d1
Thomas Camminady <0milieux_member@icloud.com>
2023-09-28 14:22:08 +0200

    Refactoring.

--- stra2ical/app.py
+++ stra2ical/app.py
@@ -1,134 +1,29 @@
-import json
-import logging
-import os
-import time
-from datetime import datetime
-from pathlib import Path
-
-import numpy as np
-import polars as pl
-import requests
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
-from icalendar import Calendar, Event
+
+from stra2ical.datamanager import DataManager
 
 app = FastAPI()
 
 
 @app.get("/")
-def status() -> str:
-    return "Online."
+def hello_world() -> str:
+    """Print a "Hello, World!" message."""
+    return "Hello, World!"
 
 
-@app.get("/calendar", response_class=PlainTextResponse)
-async def calendar():
-    await download(1)  # Download latest page of activities daily
-    create_calendar()  # Create a new ics file
-    with open("calendar/Strava.ics", encoding="UTF-8") as f:
+@app.get("/strava_calendar", response_class=PlainTextResponse)
+async def strava_calendar():
+    """Receive a calendar string."""
+    # Set up a DataManager object.
+    data_manager = DataManager()
+    # Pull the latest 10 activities from Strava.
+    # If this calendar is refreshed daily, this should be enough
+    # unless you would hypothetically do more than 10 activities per day.
+    await data_manager.download_activities_from_page(page=1, per_page=10)
+    # Using the downloaded data, we create a .ics file on disk.
+    data_manager.write_ics()
+    # Return the content of the .ics file as a string so that you can subscribe
+    # to this calendar.
+    with open(data_manager.full_calendar_path, encoding="UTF-8") as f:
         return f.read()  # Return ics file as string.
-
-
-def assemble_dataframe() -> pl.DataFrame:
-    return (
-        pl.scan_parquet("activities/*.parquet")
-        .with_columns(
-            pl.col("start_date").str.strptime(pl.Datetime),
-            (1e6 * pl.col("elapsed_time")).cast(pl.Duration).alias("elapsed"),
-        )
-        .with_columns((pl.col("start_date") + pl.col("elapsed")).alias("end_date"))
-        .select("name", "start_date", "end_date", "distance")
-        .unique()
-        .sort("start_date", descending=True)
-        .collect()
-    )
-
-
-def create_calendar() -> str:
-    df = assemble_dataframe()
-    logging.info("Collected dataframe.")
-    cal = Calendar()
-    cal.add("prodid", "-//Strava Activities//")
-    cal.add("version", "2.0")
-
-    successful_event_counter = 0
-    for row in df.iter_rows(named=True):
-        try:
-            event = Event()
-            summary = row["name"]
-            if np.isfinite(row["distance"]) and row["distance"] > 0:
-                summary += f""" ({np.round(row["distance"]/1000,1)}km)"""
-            event.add("summary", summary)
-            event.add("dtstart", row["start_date"])
-            event.add("dtend", row["end_date"])
-            cal.add_component(event)
-            successful_event_counter += 1
-        except Exception as exception:
-            logging.error(exception)
-    message = f"Created {successful_event_counter} events."
-    logging.info(message)
-
-    # Write to disk
-    directory = Path.cwd() / "calendar"
-    directory.mkdir(parents=True, exist_ok=True)
-    with open(os.path.join(directory, "Strava.ics"), "wb") as f:
-        f.write(cal.to_ical())
-    logging.info("Writing to Stava.ics completed.")
-    return message
-
-
-async def download(page: int) -> dict[str, int]:
-    update_token()
-    # Get the tokens from file to connect to Strava
-    with open("stra2ical/.strava_tokens.json", encoding="UTF-8") as json_file:
-        strava_tokens = json.load(json_file)
-        access_token = strava_tokens["access_token"]
-
-    url = f"https://www.strava.com/api/v3/athlete/activities?per_page=200&page={page}"
-    r = requests.get(url + "&access_token=" + access_token, timeout=60)
-    if r.status_code == 200:
-        # Get the current datetime
-        # Convert the datetime to a filename-friendly format with milliseconds
-        now = datetime.now().strftime("%Y%m%dT%H%M%S%f")[
-            :-3
-        ]  # Keep only 3 decimal places for milliseconds
-
-        pl.from_dicts(r.json(), infer_schema_length=None).write_parquet(
-            f"activities/{now}.parquet"
-        )
-    return {"status": r.status_code}
-
-
-async def download_all():
-    for page in range(1, 200):
-        try:
-            await download(page)
-        except Exception:
-            break
-
-
-def update_token():
-    # Get the tokens from file to connect to Strava
-    with open("stra2ical/.strava_tokens.json", encoding="UTF-8") as json_file:
-        strava_tokens = json.load(json_file)
-    with open("stra2ical/.app.json", encoding="UTF-8") as json_file:
-        app_tokens = json.load(json_file)
-
-    # If access_token has expired then
-    # use the refresh_token to get the new access_token
-    if strava_tokens["expires_at"] < time.time():
-        # Make Strava auth API call with current refresh token
-        response = requests.post(
-            url="https://www.strava.com/oauth/token",
-            data={
-                "client_id": app_tokens["client_id"],
-                "client_secret": app_tokens["client_secret"],
-                "grant_type": "refresh_token",
-                "refresh_token": strava_tokens["refresh_token"],
-            },
-            timeout=60,
-        )
-        # Save response as json in new variable
-        new_strava_tokens = response.json()
-        # Save new tokens to file
-        with open("stra2ical/.strava_tokens.json", "w", encoding="UTF-8") as f:
-            json.dump(new_strava_tokens, f)
---
+++ stra2ical/datamanager.py
@@ -0,0 +1,101 @@
+import os
+from datetime import datetime
+from pathlib import Path
+
+import numpy as np
+import polars as pl
+import requests
+from icalendar import Calendar, Event
+
+from stra2ical.authenticator import Authenticator
+from stra2ical.utils.logger import logger
+
+
+class DataManager:
+    def __init__(
+        self,
+        location: str = "activities",
+        calendar_root: str = "calendar",
+        ics_filename: str = "Strava.ics",
+    ):
+        self.__location = location
+        self.__calender_root = calendar_root
+        self.__ics_filename = ics_filename
+        directory = Path.cwd() / self.__calender_root
+        directory.mkdir(parents=True, exist_ok=True)
+        self.full_calendar_path = os.path.join(directory, self.__ics_filename)
+
+    def assemble_data(self) -> pl.DataFrame:
+        """Combine parquet files into dataframe."""
+        df = (
+            pl.scan_parquet(f"{self.__location}/*.parquet")
+            .with_columns(
+                pl.col("start_date").str.strptime(pl.Datetime),
+                (1e6 * pl.col("elapsed_time")).cast(pl.Duration).alias("elapsed"),
+            )
+            .with_columns((pl.col("start_date") + pl.col("elapsed")).alias("end_date"))
+            .select("name", "start_date", "end_date", "distance")
+            .unique()
+            .sort("start_date", descending=True)
+            .collect()
+        )
+        return df
+
+    def df_to_events(self, df: pl.DataFrame) -> list[Event]:
+        """Turn dataframe into events."""
+        events = []
+        for row in df.iter_rows(named=True):
+            try:
+                event = Event()
+                summary = row["name"]
+                if np.isfinite(row["distance"]) and row["distance"] > 0:
+                    summary += f""" ({np.round(row["distance"]/1000,1)}km)"""
+                event.add("summary", summary)
+                event.add("dtstart", row["start_date"])
+                event.add("dtend", row["end_date"])
+                events.append(event)
+            except Exception as exception:
+                logger.error(exception)
+        return events
+
+    def write_ics(self) -> None:
+        """Write events to .ics."""
+        df = self.assemble_data()
+        cal = Calendar()
+        cal.add("prodid", "-//Strava Activities//")
+        cal.add("version", "2.0")
+
+        events = self.df_to_events(df)
+        for event in events:
+            cal.add_component(event)
+
+        message = f"Created {len(events)} events."
+        logger.info(message)
+
+        # Write to disk
+        with open(self.full_calendar_path, "wb") as f:
+            f.write(cal.to_ical())
+        logger.info("Writing to Stava.ics completed.")
+
+    async def download_activities_from_page(
+        self, page: int, per_page: int = 200
+    ) -> dict[str, int]:
+        """Download recent activities."""
+        auth = Authenticator()
+        auth.update_token()
+        # Get the tokens from file to connect to Strava
+        access_token = str(auth.tokens_strava["access_token"])
+
+        url = f"https://www.strava.com/api/v3/athlete/activities?per_page={per_page}&page={page}"
+        result = requests.get(url + "&access_token=" + access_token, timeout=60)
+        if result.status_code == 200:
+            now = datetime.now().strftime("%Y%m%dT%H%M%S%f")[:-3]
+            pl.from_dicts(result.json(), infer_schema_length=None).write_parquet(
+                f"activities/{now}.parquet"
+            )
+        return {"status": result.status_code}
+
+    async def download_all_activities(self, max_page: int = 200):
+        """Download all activities."""
+        for page in range(1, max_page):
+            await self.download_activities_from_page(page)
